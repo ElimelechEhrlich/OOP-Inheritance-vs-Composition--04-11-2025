afd7da2fd9ecb0c127e9539cfd5aaa240e3663db
Elimelech Ehrlich <ee525566@gmail.com>
2025-11-04 09:54:03 +0200

    create vehicles class and subclasses

---
+++ vehicles.py
@@ -0,0 +1,17 @@
+class Vehicle:
+    def __init__(self,  max_speed, brand, model):
+        self.max_speed =  max_speed
+        self.brand = brand
+        self.model = model
+    
+    def drive(self):
+        print (f'the max speed of {self.brand}{self.model} is {self.max_speed}')
+
+class Car(Vehicle):
+    def drive(self):
+        return super().drive()
+
+class Motorcycle(Vehicle):
+    def drive(self):
+        return super().drive()
+
